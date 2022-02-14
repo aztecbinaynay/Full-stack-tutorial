598939a5ba75cc077aa36d1b579aec02d7bca240
John Edward Binay <qjeabinay@tip.edu.ph>
2022-02-14 12:48:25 +0800

    2022-02-14

--- ZTM Web Dev/11. JavaScript/10.14 arrays exercise/exercise6-SOLUTIONS.js
+++ ZTM Web Dev/11. JavaScript/10.14 arrays exercise/exercise6-SOLUTIONS.js
@@ -3,23 +3,24 @@ var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 // 1. Remove the Banana from the array.
 array.shift();
-
+console.log(array)
 // 2. Sort the array in order. 
 array.sort();
-
+console.log(array)
 // 3. Put "Kiwi" at the end of the array.
 array.push("Kiwi");
-
+console.log(array)
 // 4. Remove "Apples" from the array.
 array.splice(0, 1);
-
+console.log(array)
 // 5. Sort the array in reverse order. 
 array.reverse();
-
+console.log(array)
 // using this array, 
-// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 // access "Oranges".
 // array2[1] -> ["Apples", ["Oranges"], "Blueberries"]
 // array[1][1] -> ["Oranges"]
 // array[1][1][0] -> "oranges"
 array2[1][1][0];
+console.log(array2[1][1][0])
